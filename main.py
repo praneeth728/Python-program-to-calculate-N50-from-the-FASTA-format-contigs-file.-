9228e17a6502dcb70909a055ca4521b2f9a7c9f1
praneeth728 <66771536+praneeth728@users.noreply.github.com>
2023-02-26 01:03:28 -0600

    Update main.py

--- main.py
+++ main.py
@@ -4,16 +4,23 @@ from Bio import SeqIO
 seq_file = "C:/Users/kandu/OneDrive/Desktop/BCB/info2/hands_on_assignment3/assembly_spades/contigs.fasta"
 seq_records = SeqIO.parse(seq_file, "fasta")
 
-# Initialize variables to keep track of the largest contig and its length
+# Initialize variables to keep track of the largest contig,its length,number of contigs,total length
 largest_contig_id = None
 largest_contig_len = 0
+no_of_contigs=0
+total_lenght=0
+#iterating through records
 
-# Iterate over the sequence records and find the largest contig
+# Iterate over the sequence records and find the largest contig,number of contigs,total length
 for seq_record in seq_records:
+    no_of_contigs+=1
+    total_lenght+= len(seq_record.seq)
     if len(seq_record.seq) > largest_contig_len:
         largest_contig_id = seq_record.id
         largest_contig_len = len(seq_record.seq)
 
-# Print the largest contig ID and length
+# Print the largest contig ID,length,number of contigs,total length
+print("Number of Contigs:", no_of_contigs)
+print("Total Contig Length:", total_lenght)
 print("Largest Contig ID:", largest_contig_id)
 print("Largest Contig Length:", largest_contig_len)
